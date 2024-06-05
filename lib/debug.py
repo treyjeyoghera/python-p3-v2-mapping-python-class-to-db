cece978a62c7b421734161cace76490dde55d485
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-06-05 10:22:03 +0300

    p

--- lib/debug.py
+++ lib/debug.py
@@ -7,3 +7,6 @@ from department import Department
 import ipdb
 
 ipdb.set_trace()
+https://moringa.instructure.com/courses/704/assignments/50679?module_item_id=108093#:~:text=ipdb%3E%20%20Department.create_table()
+
+ipdb>  CURSOR.execute("PRAGMA table_info(departments)").fetchall()
